de5fb076bdef313ebb9ce55bff275f59c7a12c9e
Dante Ventura <magicman1928@gmail.com>
2022-10-21 20:31:19 -0500

    Prototype rotation (#1)
    * add Maze scene and rotation script, 1st itter
    
    * add prototype maze with collisions for reference
    
    * remove import junk
    
    * fix gitignore mispell
    
    Co-authored-by: Dante Ventura <dante.g.ventura@gmail.com>

---
+++ MazeBase.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+export var rotation_speed = 0.005
+export var max_rotation_speed = 0.05
+var rotation_rad = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func get_input():
+	if (!Input.is_action_pressed("gameplay_rotate_right") and
+		!Input.is_action_pressed("gameplay_rotate_left")):
+		rotation_rad = move_toward(rotation_rad, 0, rotation_speed)
+		
+	if Input.is_action_pressed("gameplay_rotate_right"):
+		rotation_rad = max(rotation_rad-rotation_speed, -max_rotation_speed)
+	if Input.is_action_pressed("gameplay_rotate_left"):
+		rotation_rad = min(rotation_rad+rotation_speed, max_rotation_speed)
+
+func _physics_process(delta):
+	get_input()
+	rotate(rotation_rad)
