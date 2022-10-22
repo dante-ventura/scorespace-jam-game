d544bef8e80282b53bacd30a383248d365302cbe
thecodemonkey-sudo <80733120+thecodemonkey-sudo@users.noreply.github.com>
2022-10-22 00:23:39 -0500

    Pause (#5)
    * adding timer
    
    * fix dante
    
    * adding timer
    
    * adding timer
    
    * adding timer
    
    * adding pause
    
    * adding pause
    
    Co-authored-by: Dante Ventura <magicman1928@gmail.com>

--- scripts/SettingsScene.gd
+++ scripts/SettingsScene.gd
@@ -4,6 +4,9 @@ func _on_MusicSlider_value_changed(_value):
 # warning-ignore:return_value_discarded
 	pass # Replace with function body.
 
+func _input(event):
+	if event.is_action_pressed("gameplay_pause"):
+		get_tree().change_scene("res://scenes/HomeBase.tscn")
 
 func _on_SfxSlider_value_changed(_value):
 # warning-ignore:return_value_discarded
