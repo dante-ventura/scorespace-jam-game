24e84d2327845a37b3f62630799650ef46e253ae
Kevin <kevintian41@gmail.com>
2022-10-21 23:21:17 -0500

    adding timer

--- Timer.gd
+++ Timer.gd
@@ -3,11 +3,6 @@ extends Label
 var time = 0
 var timer_on = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	timer_on = true
@@ -21,4 +16,3 @@ func _process(delta):
 	var mins = fmod(time, 60*60) / 60
 	var time_passed = "%02d : %02d" % [mins,secs]
 	text = time_passed
-	pass
