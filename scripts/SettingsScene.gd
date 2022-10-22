589fc39d9cba7227121010a927d2a5b8b8b83534
thecodemonkey-sudo <80733120+thecodemonkey-sudo@users.noreply.github.com>
2022-10-21 23:23:39 -0500

    adding timer (#4)
    * adding timer
    
    * fix dante
    
    * adding timer
    
    * adding timer
    
    * adding timer

---
+++ scripts/SettingsScene.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+func _on_MusicSlider_value_changed(_value):
+# warning-ignore:return_value_discarded
+	pass # Replace with function body.
+
+
+func _on_SfxSlider_value_changed(_value):
+# warning-ignore:return_value_discarded
+	pass # Replace with function body.
+
+
+func _on_ButtonHome_pressed():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://scenes/HomeBase.tscn")
