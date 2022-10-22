ecdc1b36f7e8ec842d3ec268822e0d1f35768732
Kevin <kevintian41@gmail.com>
2022-10-21 23:50:31 -0500

    adding pause

--- scripts/Pause.gd
+++ scripts/Pause.gd
@@ -2,6 +2,5 @@ extends Control
 
 func _input(event):
 	if event.is_action_pressed("gameplay_pause"):
-		var new_pause_state = not get_tree().paused
 		get_tree().paused = not get_tree().paused
-		visible = new_pause_state
+		visible = get_tree().paused
