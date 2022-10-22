f0ca9a181c35cdefeaae62420e16a9c079daec1f
Kevin <kevintian41@gmail.com>
2022-10-21 23:05:05 -0500

    adding timer

--- scripts/SettingsScene.gd
+++ scripts/SettingsScene.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-func _on_MusicSlider_value_changed(value):
+func _on_MusicSlider_value_changed(_value):
 	pass # Replace with function body.
 
 
-func _on_SfxSlider_value_changed(value):
+func _on_SfxSlider_value_changed(_value):
 	pass # Replace with function body.
 
 
 func _on_ButtonHome_pressed():
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/HomeBase.tscn")
