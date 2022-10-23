1b0c93fb3b320da80d4823e251248f0c226d565b
Kevin <kevintian41@gmail.com>
2022-10-22 19:17:27 -0500

    stupid leaderboard

---
+++ scripts/LeaderboardBase.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://scenes/HomeBase.tscn")
+
+func _input(event):
+	if event.is_action_pressed("gameplay_pause"):
+	# warning-ignore:return_value_discarded
+		get_tree().change_scene("res://scenes/HomeBase.tscn")
